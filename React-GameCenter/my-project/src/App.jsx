b4faa09f5cd85601a330a6965056606483f7ceea
Jack Delamou <jackdelamou.97@gmail.com>
2023-10-17 19:09:13 +0200

    Project setup
    Project setup completed

---
+++ React-GameCenter/my-project/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-lg justify-center flex-1 items-center text-red-800">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default App;
